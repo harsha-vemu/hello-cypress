c2e911d4eea3dfa5bffb68aa6a1590510fed1916
Harsha Vemu <h.vemu@MBPC02YM7HVLVCG.local>
2022-04-04 13:46:16 +0200

    CI for Browserstack init

--- cypress/integration/vorwerk.spec.js
+++ cypress/integration/vorwerk.spec.js
@@ -5,20 +5,19 @@ describe('Vorwerk Regresstion Tests', function () {
         cy.get('select').select('Germany').should('have.value', '1');
         cy.get('select').select('Germany'); // Select DE
         cy.get('.country-selector__button').click()
-        cy.url().should('eq', 'https://www.vorwerk.com/de/de/c/home') // Land at Vorwerk DE page
     })
 
-    it('Adds Thermomix product to Cart', function () {
-        // cy.get('#onetrust-accept-btn-handler').click() // Accept Cookies
+    it('Adds TM6 to Cart & Fill advisor Form', function () {
+        cy.url().should('eq', 'https://www.vorwerk.com/de/de/c/home') // Land at Vorwerk DE page
+        cy.get('#onetrust-accept-btn-handler').click() // Accept Cookies
+
         cy.contains('Produkte').click() // Click Products menu
         cy.contains('Thermomix® TM6').click() // Select TM6®
-        cy.scrollTo(0, 500) // Scroll the window 500px down
-        cy.contains('In den Warenkorb').click() // Add to Vart
-    })
 
-    it('Fills advisor form & goto Checkout', function() {
-        cy.get('#onetrust-accept-btn-handler').click() // Accept Cookies
-        cy.contains('Accept All Cookies').click() // Accept Cookies
+        // cy.get('#onetrust-accept-btn-handler').click() // Accept Cookies
+        cy.contains('In den Warenkorb').click() // Add to Cart
+
+
         cy.get('#advisorsearch-first-name').type('Hilde');
         cy.get('#advisorsearch-last-name').type('Holzer');
         cy.get('#advisorsearch-street').type('Brahmsallee');
@@ -26,6 +25,5 @@ describe('Vorwerk Regresstion Tests', function () {
         cy.get('#advisorsearch-postal').type('20144');
         cy.get('#advisorsearch-city').type('Hamburg');
         cy.contains('Bestellung fortsetzen').click();
-        cy.contains('Bestellung ohne Willkommens-Service fortsetzen').click();
     })
 })
